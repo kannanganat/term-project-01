026be1d9241b32bfade8220d284159affd3ac13a
kannanganat <arun.kannanganat@utah.edu>
2014-04-10 13:45:05 -0600

    pls refer documentation for details
     on the code and project description. this is just a sample

--- ribosomal-all-interactions.py
+++ ribosomal-all-interactions.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
-# This is an automatically generated script to run your query
-# to use it you will require the intermine python client.
-# To install the client, run the following command from a terminal:
-#
-#     sudo easy_install intermine
-#
-# For further documentation you can visit:
-#     http://www.intermine.org/wiki/PythonClient
+# please refer documentation of the project for details on the code. 
+
 
 # The following two lines will be needed in every python script:
 from intermine.webservice import Service
